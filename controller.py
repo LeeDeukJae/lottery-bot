da683b790b673bb9b46622494ba59ce9fca08cec
LeeDeukJae <43134477+LeeDeukJae@users.noreply.github.com>
2025-02-10 16:49:36 +0900

    Update controller.py

--- controller.py
+++ controller.py
@@ -9,23 +9,12 @@ import notification
 import time
 
 
-def buy_lotto645(authCtrl: auth.AuthController, cnt: int, mode: str):
-
-    # 직접 선택한 로또 번호 (최대 5개)
-    manual_numbers = [
-        [2, 8, 12, 29, 38, 39],  # 첫 번째 슬롯 (완전 수동)
-        [2, 8, 12, 29, 38, 39],  # 첫 번째 슬롯 (완전 수동)
-        [17, 19],                # 두 번째 슬롯 (반자동: 나머지 자동)
-        [40, 43, 44],             # 네 번째 슬롯 (반자동)
-        None                      # 다섯 번째 슬롯 (완전 자동)
-    ]
-    
+def buy_lotto645(authCtrl: auth.AuthController, cnt: int, mode: str, manual_numbers: list):
     lotto = lotto645.Lotto645()
-    _mode = "MANUAL"
+    _mode = lotto645.Lotto645Mode[mode.upper()]
     response = lotto.buy_lotto645(authCtrl, cnt, _mode, manual_numbers)
     response['balance'] = lotto.get_balance(auth_ctrl=authCtrl)
     return response
-
 def check_winning_lotto645(authCtrl: auth.AuthController) -> dict:
     lotto = lotto645.Lotto645()
     item = lotto.check_winning(authCtrl)
@@ -72,8 +61,8 @@ def check():
 
     time.sleep(10)
     
-    response = check_winning_win720(globalAuthCtrl)
-    send_message(0, 1, response=response, webhook_url=discord_webhook_url)
+    # response = check_winning_win720(globalAuthCtrl)
+    # send_message(0, 1, response=response, webhook_url=discord_webhook_url)
 
 def buy(): 
     
@@ -84,12 +73,21 @@ def buy():
     count = int(os.environ.get('COUNT'))
     slack_webhook_url = os.environ.get('SLACK_WEBHOOK_URL') 
     discord_webhook_url = os.environ.get('DISCORD_WEBHOOK_URL')
-    mode = "AUTO"
+    mode = "MANUAL"
+
+    # 직접 선택한 로또 번호 (최대 5개)
+    manual_numbers = [
+        [2, 8, 12, 29, 38, 39],  # 첫 번째 슬롯 (완전 수동)
+        [2, 8, 12, 29, 38, 39],  # 첫 번째 슬롯 (완전 수동)
+        [17, 19],                # 두 번째 슬롯 (반자동: 나머지 자동)
+        [40, 43, 44],             # 네 번째 슬롯 (반자동)
+        None                      # 다섯 번째 슬롯 (완전 자동)
+    ]
 
     globalAuthCtrl = auth.AuthController()
     globalAuthCtrl.login(username, password)
 
-    response = buy_lotto645(globalAuthCtrl, count, mode) 
+    response = buy_lotto645(globalAuthCtrl, count, mode, manual_numbers) 
     send_message(1, 0, response=response, webhook_url=discord_webhook_url)
 
     time.sleep(10)
